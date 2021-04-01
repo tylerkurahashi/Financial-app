8762e58443c3f8f0082593b45ea1e04b310ddbd4
tylerkurahashi <tyler.kurahashi@gmail.com>
2021-04-01 23:48:09 +0900

    home

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-const ejs = require('ejs');
+const express = require('express');
+const ejsMate = require('ejs-mate');
 const mongoose = require('mongoose');
+
+const app = express();
+
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', async(req, res) => {
+    res.render('home')
+});
+
+app.listen(3000, () => {
+    console.log('Serving on 3000')
+});

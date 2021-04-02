3e076a10d43d26669325096c85dd1ac069ac6e30
tylerkurahashi <tyler.kurahashi@gmail.com>
2021-04-02 23:32:57 +0900

    wip

---
+++ scripts/pie_chart.js
@@ -0,0 +1,23 @@
+// Mock Data
+var data = [
+    {
+     value: 300,
+     color:"#F7464A",
+     highlight: "#FF5A5E",
+     label: "Red"
+    },
+    {
+     value: 50,
+     color: "#46BFBD",
+     highlight: "#5AD3D1",
+     label: "Green"
+    },
+    {
+     value: 100,
+     color: "#FDB45C",
+     highlight: "#FFC870",
+     label: "Yellow"
+    }
+   ];
+   
+   var myChart = new Chart(document.getElementById("mycanvas").getContext("2d")).Doughnut(data);

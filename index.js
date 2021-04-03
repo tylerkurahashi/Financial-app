ca1b7df59d70bf90767249cfabedd792393f0ac5
tylerkurahashi <tyler.kurahashi@gmail.com>
2021-04-03 10:32:31 +0900

    pie chart working

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path')
 const ejsMate = require('ejs-mate');
 const mongoose = require('mongoose');
 
@@ -8,6 +9,7 @@ app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', async(req, res) => {
     res.render('home')

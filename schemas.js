3352faebdc152112c7aa328c596be6508d2ab0ca
tylerkurahashi <tyler.kurahashi@gmail.com>
2021-04-04 23:54:05 +0900

    validation schema modified

--- schemas.js
+++ schemas.js
@@ -3,8 +3,8 @@ const { number } = require('joi');
 
 module.exports.historySchema = Joi.object({
     history: Joi.object({
-        month: Joi.number().required().min(0),
-        day: Joi.number().required().min(0),
+        month: Joi.number().required().min(0).max(12),
+        day: Joi.number().required().min(0).max(31),
         content: Joi.string().required(),
         category: Joi.string().required(),
         price: Joi.number().required().min(0)
